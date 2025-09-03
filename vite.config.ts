6220f510d694f4f0d180e523ab62253c3de1b303
Danilo Hoffmann <dhhyi@aol.com>
2025-09-03 09:34:00 +0000

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import { type ManifestOptions, VitePWA } from "vite-plugin-pwa";
+import manifest from "./manifest.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: manifest as unknown as ManifestOptions,
+    }),
+  ],
   base: "/gin-rummy-scoreboard/",
 });
