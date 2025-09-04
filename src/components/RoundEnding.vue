4689503729c165113307d67c926658bfbe004079
Danilo Hoffmann <dhhyi@aol.com>
2025-09-04 11:39:41 +0000

    remove points from round ending screen

--- src/components/RoundEnding.vue
+++ src/components/RoundEnding.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type Context, getScoreForPlayer } from "../game-machine";
+import { type Context } from "../game-machine";
 
 const props = defineProps({
   context: { type: Object as () => Context, required: true },
@@ -10,10 +10,6 @@ const emit = defineEmits<{
   (e: "player-two-ends"): void;
 }>();
 
-function score(player: 1 | 2) {
-  return getScoreForPlayer(player, props.context.scoring);
-}
-
 const round = props.context.rounds + 1;
 </script>
 
@@ -22,10 +18,10 @@ const round = props.context.rounds + 1;
   <div class="flex flex-col gap-8">
     <h2>Wer hat die Runde beendet?</h2>
     <button @click="emit('player-one-ends')">
-      {{ context.playerOne }} ({{ score(1) }})
+      {{ context.playerOne }}
     </button>
     <button @click="emit('player-two-ends')">
-      {{ context.playerTwo }} ({{ score(2) }})
+      {{ context.playerTwo }}
     </button>
   </div>
 </template>
