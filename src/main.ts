daef1ad7490158064e63d214668e9e6da00c3345
Danilo Hoffmann <dhhyi@aol.com>
2025-09-07 16:49:34 +0000

    auto-update app via service worker

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
+import { registerSW } from "virtual:pwa-register";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
 createApp(App).mount("#app");
+
+const intervalMS = 60 * 1000;
+
+registerSW({
+  onRegistered(r) {
+    if (r) {
+      setInterval(() => {
+        r.update();
+      }, intervalMS);
+    }
+  },
+});
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,3 @@
+/// <reference types="vite-plugin-pwa/client" />
+
 declare const GIT_HASH: string;
