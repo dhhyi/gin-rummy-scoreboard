70f62f47beb32c75f2f78cca7a465c37e147d8cb
Danilo Hoffmann <dhhyi@aol.com>
2026-02-23 16:31:48 +0000

    upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default typescriptEslint.config(
           extensions: [".ts", ".vue"],
         },
       ],
+      "no-useless-assignment": "off",
     },
     settings: {
       "vue-i18n": {
